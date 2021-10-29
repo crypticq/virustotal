d259495781fa53dcc14554d53f0408f658f4347a
crypticq <85430867+crypticq@users.noreply.github.com>
2021-10-29 11:23:02 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import requests
+url = 'https://www.virustotal.com/vtapi/v2/file/scan'
+params = {'apikey': '<apikey>'}
+files = {'file': ('myfile.exe', open('myfile.exe', 'rb'))}
+response = requests.post(url, files=files, params=params)
+print(response.json())
