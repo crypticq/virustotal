7c5bffce57cb0527f1a0ee55e6963c4f8f6475a8
crypticq <85430867+crypticq@users.noreply.github.com>
2021-10-29 11:24:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
+api_key = input(" virustotal key?: ")
 url = 'https://www.virustotal.com/vtapi/v2/file/scan'
-params = {'apikey': '<apikey>'}
+params = {'apikey': api_key}
 files = {'file': ('myfile.exe', open('myfile.exe', 'rb'))}
 response = requests.post(url, files=files, params=params)
 print(response.json())
